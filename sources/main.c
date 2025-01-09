3be4b73005aa05cc60b0609ba49c65e2c96e48f4
ghambrec <ghambrec@student.42heilbronn.de>
2025-01-09 15:54:26 +0100

    first try with forks and env

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 13:27:17 by ghambrec          #+#    #+#             */
-/*   Updated: 2025/01/09 13:38:17 by ghambrec         ###   ########.fr       */
+/*   Updated: 2025/01/09 15:52:44 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 
 int	main(void)
 {
-	ft_printf("Hallo!");
-	return (0);
+	int pid = fork();
+	ft_printf("%i\n", pid);
+	wait(NULL);
+	
+	return (EXIT_SUCCESS);
 }
+
+// #include <stdio.h>
+
+// int main(int ac, char **av, char **env) {
+//     // Kommandozeilenargumente ausgeben
+//     printf("Anzahl der Argumente: %d\n", ac);
+//     for (int i = 0; i < ac; i++) {
+//         printf("Argument %d: %s\n", i, av[i]);
+//     }
+
+//     // Umgebungsvariablen ausgeben
+//     printf("\nUmgebungsvariablen:\n");
+//     for (int i = 0; env[i] != NULL; i++) {
+//         printf("%s\n", env[i]);
+//     }
+
+//     return 0;
+// }
