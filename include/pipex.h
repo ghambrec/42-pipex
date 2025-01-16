c7934d3472cdf124488a9bdb8e55b2904b2f33f7
ghambrec <ghambrec@student.42heilbronn.de>
2025-01-16 11:38:05 +0100

    splitted the big main into different functions

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 13:29:25 by ghambrec          #+#    #+#             */
-/*   Updated: 2025/01/09 13:29:55 by ghambrec         ###   ########.fr       */
+/*   Updated: 2025/01/16 11:11:09 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PIPEX_H
 
 # include "../sources/myLibft/include/my_libft.h"
+# include <stdio.h>
 
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 13:27:17 by ghambrec          #+#    #+#             */
-/*   Updated: 2025/01/15 20:24:45 by ghambrec         ###   ########.fr       */
+/*   Updated: 2025/01/16 11:37:38 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,82 +61,86 @@ char	*get_path(char *cmd, char **env)
 	return (free_split(paths), NULL);
 }
 
+void	exec_cmd(char *cmd, char **env)
+{
+	char	**args;
+	char	*path;
+
+	args = ft_split(cmd, ' ');
+	path = get_path(args[0], env);
+	if (!path)
+	{
+		free_split(args);
+		ft_putendl_fd("Could not find the program", STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	if (execve(path, args, env) == -1)
+	{
+		ft_putstr_fd("Error executing command ", STDERR_FILENO);
+		ft_putstr_fd(args[0], STDERR_FILENO);
+		perror(" ");
+		free_split(args);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	child(char *infile, char *cmd, int *pipefd, char **env)
+{
+	int	infile_fd;
+
+	infile_fd = open(infile, O_RDONLY);
+	if (infile_fd == -1)
+	{
+		perror("Error opening infile");
+		exit(EXIT_FAILURE);
+	}
+	dup2(infile_fd, STDIN_FILENO);
+	close(infile_fd);
+	dup2(pipefd[1], STDOUT_FILENO);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	exec_cmd(cmd, env);
+}
+
+void	parent(char *outfile, char *cmd, int *pipefd, char **env)
+{
+	int	outfile_fd;
+
+	outfile_fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (outfile_fd == -1)
+	{
+		ft_printf("Error opening outfile");
+		exit(EXIT_FAILURE);
+	}
+	// dup2(outfile_fd, STDOUT_FILENO); //for testing print in the terminal not in the file
+	close(outfile_fd);
+	dup2(pipefd[0], STDIN_FILENO);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	exec_cmd(cmd, env);
+}
 
 int	main(int argc, char **argv, char **env)
 {
 	int	pipefd[2];
 	int	error_check;
 	int	pid1;
-	int	pid2;
 
-	// if (argc != 5)
-	// 	return (EXIT_FAILURE);
+	if (argc != 5)
+	{
+		ft_putendl_fd("Bad arguments! Expection: ./pipex file1 cmd1 cmd2 file2", STDERR_FILENO);
+		return (EXIT_FAILURE);	
+	}
 	error_check = pipe(pipefd);
 	if (error_check == -1)
-		return (EXIT_FAILURE);
-
+		return (perror("Error opening pipe"), EXIT_FAILURE);
 	pid1 = fork();
 	if (pid1 < 0)
-		return (EXIT_FAILURE);
+		return (perror("Error opening child-process"), EXIT_FAILURE);
 	if (pid1 == 0)
 	{
-		// in the child process
-		error_check = open(argv[1], O_RDONLY);
-		if (error_check == -1)
-		{
-			ft_printf("error opening infile");
-			return (EXIT_FAILURE);
-		}
-		dup2(error_check, STDIN_FILENO);
-		close(error_check);
-		dup2(pipefd[1], STDOUT_FILENO);
-		close(pipefd[0]);
-		close(pipefd[1]);
-
-		char **args = ft_split(argv[2], ' ');
-		char *path;
-		path = get_path(args[0], env);
-		if (execve(path, args, env) == -1)
-		{
-			ft_putstr_fd("error executing command: ", STDERR_FILENO);
-			ft_putendl_fd(args[0], STDERR_FILENO);
-			free_split(args);
-			return (EXIT_FAILURE);
-		}
+		child(argv[1], argv[2], pipefd, env);
 	}
-	pid2 = fork();
-	if (pid2 < 0)
-		return (EXIT_FAILURE);
-	if (pid2 == 0)
-	{
-		error_check = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-		if (error_check == -1)
-		{
-			ft_printf("error opening outfile");
-			return (EXIT_FAILURE);
-		}
-		dup2(error_check, STDOUT_FILENO);
-		close(error_check);
-		dup2(pipefd[0], STDIN_FILENO);
-		close(pipefd[0]);
-		close(pipefd[1]);
-
-		char **args = ft_split(argv[3], ' ');
-		char *path;
-		path = get_path(args[0], env);
-		if (execve(path, args, env) == -1)
-		{
-			ft_putstr_fd("error executing command: ", STDERR_FILENO);
-			ft_putendl_fd(args[0], STDERR_FILENO);
-			free_split(args);
-			return (EXIT_FAILURE);
-		}
-	}
-
-
-	close(pipefd[0]);
-	close(pipefd[1]);
+	parent(argv[4], argv[3], pipefd, env);
 	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
-
 }
