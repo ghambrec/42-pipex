04f13b86ebe5ad54016a3969e94daaa7737b47ad
ghambrec <ghambrec@student.42heilbronn.de>
2025-01-27 13:16:50 +0100

    added include for waitpid

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 13:29:25 by ghambrec          #+#    #+#             */
-/*   Updated: 2025/01/16 12:47:26 by ghambrec         ###   ########.fr       */
+/*   Updated: 2025/01/27 13:16:29 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "../sources/myLibft/include/my_libft.h"
 # include <stdio.h>
 # include <errno.h>
+# include <sys/wait.h>
 
 char	*get_path(char *cmd, char **env);
 void	exec_cmd(char *cmd, char **env);
